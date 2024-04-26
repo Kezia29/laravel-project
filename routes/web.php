4453ecf9711ac1198d6f12d8730a852b553c5d02
Kezia29 <berlianikpb29@gmail.com>
2024-04-26 17:09:25 +0700

    pertemuan 5

--- routes/web.php
+++ routes/web.php
@@ -2,32 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {return view('welcome');});
+use App\Http\Controllers\HomeController;
 
-Route::get('/about', function() {return view('my-about');});
-Route::get('/books/terpinjam/1', function() {return view("halaman buku");});
-Route::get('/hallo', function() {return view('hallo', ['nama' => 'IK2', 'alamat' => 'jl. tharmin 35A', 'title' => 'Hallo']);});
-Route::get('/login', function() {
-    $nama  = 'Bambang';
-    $lampu = 1;
-    $kelas = env('KELAS', 'Data kelas tidak ada');
-    $nilai = "B";
-    $perulangan_for = 100;
-    $hobi_banyak =
-        [
-        'Bermain Bola',
-        'Bermain Air',
-        'Bermain Badminton',
-        'Membaca',
-        'Memasak',
-        'Bermain musik',
-        ];
-    return view('admin.login', [
-    'nama' => $nama,
-    'kelas' => $kelas,
-    'lampu' => $lampu,
-    "nilai" => $nilai,
-    'limit' => $perulangan_for,
-    'hobbies' => $hobi_banyak,
-    ]);
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [HomeController::class, 'about']);
+Route::get('/test', [HomeController::class, 'test']);
+Route::get('/test/{id}', [HomeController::class, 'test']);
