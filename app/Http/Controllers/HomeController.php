44a32c5168e93a2c7e985a6d23087fa8bebdcdbb
Kezia29 <berlianikpb29@gmail.com>
2024-04-26 23:06:30 +0700

    pertemuan 7

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Katgori;
+use App\Models\Makanan;
 
 class HomeController extends Controller
 {
@@ -32,7 +34,22 @@ class HomeController extends Controller
             'keterangan' => $ket,
         ]);
     }
-    public function test()
+    public function view_makanan()
+    {
+        // query builder
+        // $makanans = DB::table('makanans')->get();
+
+        // eloquent
+        $makanans = Makanan::all();
+        
+        // dd($makanans)
+
+        return view('makanan', [
+            'foods' => $makanans,
+        ]);
+        
+    }
+    public function test_query_builder()
     {
         // Query Builder untuk insert 1 data ke dalam table makanans
         // DB::table('makanans')->insert([
@@ -74,5 +91,42 @@ class HomeController extends Controller
         // DB::table('makanans')->where('id', 1)->delete();
         //  Query untuk Update data dari table makanans
         return 'masuk test';
+    }
+    public function test_eloquent()
+    {
+        // SELECT DATA
+        $makanans = Makanan::all();
+
+        // dd($makanans);
+        // INSERT DATA CARA 1 
+        // Makanan::create([
+        //     'kode_makanan' => 'm001', 
+        //     'nama' => 'Arip',
+        //     'kategori' => 'tampan', 
+        //     'harga' => 100000000000, 
+        //     'ket' => 'dijual',
+        // ]);
+        // INSERT DATA CARA 2
+        // $makanan = new Makanan;
+        // $makanan->kode_makanan = 'm100';
+        // $makanan->nama = 'Kuaci';
+        // $makanan->kategori = 'makanan';
+        // $makanan->harga = 10000; 
+        // $makanan->ket = 'tersedia'; 
+        // $makanan->save();
+        // Hapus data cara 1
+        // Makanan::find('m001')->delete();
+        // Hapus data cara 2
+        // $makanan = Makanan::find('m☺01');
+        // $makanan->delete();
+        // Update data
+        // $makanan = Makanan::find('m100');
+        // $makanan->nama = 'telor asin'; 
+        // $makanan->kategori = 'snack'; 
+        // $makanan->harga = 5000;
+        // $makanan->ket  = 'tersedia';
+        // $makanan->save();
+
+        // return 'masuk test';
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/about', [HomeController::class, 'about']);
-Route::get('/test', [HomeController::class, 'test']);
+Route::get('/test_query_builder', [HomeController::class, 'test_query_builder']);
+Route::get('/test_eloquent', [HomeController::class, 'test_eloquent']);
 Route::get('/add', [HomeController::class, 'tambah']);
 Route::post('/save', [HomeController::class, 'simpan']);
+Route::get('/makanan', [HomeController::class, 'view_makanan']);
