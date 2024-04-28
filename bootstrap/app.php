83c74108414be1c91a29f05d979ac9099ae22610
Kezia29 <berlianikpb29@gmail.com>
2024-04-28 23:03:30 +0700

    pertemuan 18

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\IsAuth;
+use App\Http\Middleware\IsNotAuth;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'is.auth' => IsAuth::class,
+            'is.not.auth' => IsNotAuth::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
